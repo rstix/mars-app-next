322e90049fda93c5d365bce1991567a88acfa8c8
Roman Stix <roman.stix@digitalsunray.com>
2024-09-21 14:42:15 +0200

    properties added

--- app/page.jsx
+++ app/page.jsx
@@ -1,11 +1,12 @@
-import Link from 'next/link';
 import InfoBoxes from '@/components/info-boxes';
 import Hero from '@/components/hero';
+import HomeProperties from '@/components/home-properties';
 
 const HomePage = () => {
   return (
     <div className="">
       <Hero />
+      <HomeProperties />
       <InfoBoxes />
     </div>
   );
