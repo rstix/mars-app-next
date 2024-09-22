066942b504cbbeec945c059a4456cfb1d0e0465d
Roman Stix <roman.stix@digitalsunray.com>
2024-09-22 13:54:17 +0200

    property detail created

--- app/properties/[id]/page.jsx
+++ app/properties/[id]/page.jsx
@@ -1,10 +1,40 @@
 import connectDB from '@/config/database';
 import Property from '@/models/Property';
+import PropertyHeaderImage from '@/components/property-header-image';
+import Link from 'next/link';
+import { FaArrowLeft } from 'react-icons/fa';
+import PropertyDetails from '@/components/property-details';
 
 const PropertyPage = async ({ params }) => {
   await connectDB();
   const property = await Property.findById(params.id).lean();
-  return <div>PropertyPage {property.name}</div>;
+  return (
+    <>
+      <PropertyHeaderImage image={property.images[0]} />
+      <section>
+        <div className="container m-auto py-6 px-6">
+          <Link
+            href="/properties"
+            className="text-blue-500 hover:text-blue-600 flex items-center"
+          >
+            <FaArrowLeft className="mr-2" /> Back to Properties
+          </Link>
+        </div>
+      </section>
+      <section className="bg-blue-50">
+        <div className="container m-auto py-10 px-6">
+          <div className="grid grid-cols-1 md:grid-cols-70/30 w-full gap-6">
+            <PropertyDetails property={property} />
+            {/* <aside className="space-y-4">
+              <BookmarkButton property={property} />
+              <ShareButtons property={property} />
+              <PropertyContactForm property={property} />
+            </aside> */}
+          </div>
+        </div>
+      </section>
+    </>
+  );
 };
 
 export default PropertyPage;
